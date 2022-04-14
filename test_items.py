b53d85216c57f291ea2149b8e2e2598d8ec58097
viqto <viktorkalakun@gmail.com>
2022-04-14 23:39:24 +0500

    Language test added

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+import pytest
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    time.sleep(1)
+    add_to_basket = browser.find_element_by_class_name("btn-add-to-basket").tag_name
+    assert add_to_basket == "button", f"Кнопка {add_to_basket} не существует"
