6546e254dd2cf3ad42f5898c82f6a78760c6e9bc
viqto <103384585+viqto@users.noreply.github.com>
2022-04-14 22:11:32 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import pytest
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_button(browser):
     browser.get(link)
     time.sleep(1)
     add_to_basket = browser.find_element_by_class_name("btn-add-to-basket").tag_name
